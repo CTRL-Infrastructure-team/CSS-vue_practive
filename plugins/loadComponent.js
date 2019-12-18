3386064824df1bc94956e70db50e92b8545c2952
sunikaaa <sin.seki.ara@gmail.com>
2019-12-18 10:42:39 +0900

    git flow

--- plugins/loadComponent.js
+++ plugins/loadComponent.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 
 
 import headerComponent from '~/components/header/sample';
+import mainimg from '~/components/mainimg/sample';
 import sample from '~/components/UnderStoodCss/sample';
 
 
